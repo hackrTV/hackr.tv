2047d6eed989c65a949808cd0cfab850fc5eadf0
X E R A E N <x@hackr.tv>
2025-06-29 12:21:08 -0500

    Handle Mobile Access (#2)

--- hackr.tv.rb
+++ hackr.tv.rb
@@ -1,3 +1,4 @@
+require "browser"
 require "erb"
 require "sinatra"
 
@@ -8,7 +9,10 @@ set(
     permitted_hosts: [
       "ashlinn.net",
       "cyberpul.se",
+      "git.xeraen.net",
       "hackr.tv",
+      "links.xeraen.com",
+      "links.xeraen.net",
       "localhost",
       "rockerboy.net",
       "rockerboy.stream",
@@ -44,12 +48,16 @@ end
 
 DEFAULT_LAYOUT = :"layouts/default"
 LAYOUTS = {
-  xeraen: :"layouts/xeraen",
-  sector: :"layouts/sector"
+  mobile: :"layouts/default_mobile",
+  sector: :"layouts/sector",
+  xeraen: :"layouts/xeraen"
 }.freeze
 
 DEFAULT_RESCUE_PATH = "/".freeze
-RESCUE_PATHS = {xeraen: "/xeraen"}.freeze
+RESCUE_PATHS = {
+  xeraen: "/xeraen",
+  sector: "/"
+}.freeze
 
 ###############################################################################
 # BEGIN REDIRECTS
@@ -85,16 +93,7 @@ REDIRECTS = {
 ###############################################################################
 
 before do
-  template_by_url =
-    request
-      .path
-      .to_s
-      .split("/")
-      .compact
-      .delete_if(&:empty?)
-      .join("/")
-
-  @template = [template_by_url.to_sym, :index].delete_if(&:empty?).first
+  @request_analysis = RequestAnalysis.new(request)
   @site_key = request.path.to_s.split("/")[1].to_s.to_sym
   @redirect_map =
     REDIRECTS[request.host.to_s.downcase] ||
@@ -124,10 +123,83 @@ end
 
     break redirect(@redirect_url) unless @redirect_url.nil?
 
-    erb(LAYOUTS[@site_key] || DEFAULT_LAYOUT) do
-      erb @template
+    erb(@request_analysis.layout || LAYOUTS[@site_key] || DEFAULT_LAYOUT) do
+      erb @request_analysis.template
     end
   rescue
     redirect(RESCUE_PATHS[@site_key] || DEFAULT_RESCUE_PATH)
+  end
+end
+
+class RequestAnalysis
+  attr_reader :request
+
+  def initialize(req)
+    @request = req
+    @domain = @request.server_name.to_s.downcase
+    @browser =
+      Browser.new(
+        @request.user_agent,
+        accept_language: @request.env["HTTP_ACCEPT_LANGUAGE"]
+      )
+    @template_by_url =
+      @request
+        .path
+        .to_s
+        .split("/")
+        .compact
+        .delete_if(&:empty?)
+        .join("/")
+  end
+
+  def ashlinn?
+    @ashlinn ||= @domain.include?("ashlinn")
+  end
+
+  def hackr_tv?
+    @hackr_tv ||=
+      !ashlinn? &&
+        !sector_x? &&
+        !xeraen?
+  end
+
+  def layout
+    layout_symbol =
+      if hackr_tv?
+        :"layouts/default"
+      elsif xeraen?
+        :"layouts/xeraen"
+      elsif sector_x?
+        :"layouts/sector"
+      elsif ashlinn?
+        :"" # not necessary for now
+      end
+
+    return "#{layout_symbol.to_s}_mobile".to_sym if mobile?
+    layout_symbol
+  end
+
+  def mobile?
+    @browser.device.mobile?
+  end
+
+  def sector_x?
+    @sector_x ||=
+      @domain.include?("sector") ||
+        @request.path.to_s.include?("/sector")
+  end
+
+  def template
+    @template = [@template_by_url.to_sym, :index].delete_if(&:empty?).first
+    @template = :"mobile/#{@template}" if mobile?
+
+    @template
+  end
+
+  def xeraen?
+    @xeraen ||=
+      @domain.include?("xeraen") ||
+        @domain.include?("rockerboy") ||
+        @request.path.to_s.include?("/xeraen")
   end
 end
