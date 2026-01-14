1ba8891d27649ee340e30795f72e131869cbe11e
XERAEN <x@hackr.tv>
2026-01-14 14:44:37 -0600

    Enables Content Security Policy in prod w/nonces
      - Adds nonces to all 11 inline scripts across views:
        - High priority: grid/index, terminal/index
        - Admin: layout, codex_entries (index + form), overlay_scene_elements,
          pulse_wire, zone_playlists (2 scripts), radio_stations, tracks
      - Updates CSP config to apply to all environments (was dev/test only)
      - Removes :unsafe_inline from script_src - nonces authorize inline scripts instead
      - External CDN scripts (ActionCable, SortableJS) allowed via :https directive
      - Removes completed TODO documentation (docs/CSP_NONCES_TODO.md)
    
      This hardens the app against XSS attacks by ensuring only server-authorized
      inline scripts can execute. Injected malicious scripts will be blocked since
      attackers cannot guess the per-request nonce.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,34 +4,34 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-# CSP is only enabled in development/test for now.
-# Production requires adding nonces to inline scripts before enabling.
-# TODO: Add nonce: true to inline scripts in views, then enable CSP in production
-if Rails.env.development? || Rails.env.test?
-  Rails.application.configure do
-    config.content_security_policy do |policy|
-      policy.default_src :self
-      policy.font_src :self, :https, :data
-      policy.img_src :self, :https, :data
-      policy.media_src :self, :https, :http, :data
-      policy.object_src :none
-      policy.script_src :self, :https, :unsafe_inline, :unsafe_eval
-      policy.style_src :self, :https, :unsafe_inline
-      policy.connect_src :self, :https, :wss
-      policy.frame_src :self, "https://www.youtube.com", "https://www.youtube-nocookie.com"
+Rails.application.configure do
+  config.content_security_policy do |policy|
+    policy.default_src :self
+    policy.font_src :self, :https, :data
+    policy.img_src :self, :https, :data
+    policy.media_src :self, :https, :http, :data
+    policy.object_src :none
+    policy.style_src :self, :https, :unsafe_inline
+    policy.connect_src :self, :https, :wss
+    policy.frame_src :self, "https://www.youtube.com", "https://www.youtube-nocookie.com"
 
-      if Rails.env.development?
-        vite_host = ViteRuby.config.host_with_port
-        policy.script_src(*policy.script_src, "http://#{vite_host}")
-        policy.connect_src(*policy.connect_src, "ws://#{vite_host}", "http://#{vite_host}")
-      end
+    # Script sources - nonces handle inline scripts, so no :unsafe_inline needed
+    # External CDN scripts (ActionCable, Sortable) are allowed via :https
+    policy.script_src :self, :https
 
-      policy.script_src(*policy.script_src, :blob) if Rails.env.test?
+    if Rails.env.development?
+      # Vite dev server needs additional permissions
+      vite_host = ViteRuby.config.host_with_port
+      policy.script_src(*policy.script_src, "http://#{vite_host}", :unsafe_eval)
+      policy.connect_src(*policy.connect_src, "ws://#{vite_host}", "http://#{vite_host}")
     end
 
-    config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-    # Note: Don't include style-src in nonce_directives - it breaks inline styles
-    # even with :unsafe_inline set, because nonce presence overrides unsafe-inline
-    config.content_security_policy_nonce_directives = %w[script-src]
+    policy.script_src(*policy.script_src, :blob) if Rails.env.test?
   end
+
+  # Generate nonces for inline scripts
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  # Note: Don't include style-src in nonce_directives - it breaks inline styles
+  # even with :unsafe_inline set, because nonce presence overrides unsafe-inline
+  config.content_security_policy_nonce_directives = %w[script-src]
 end
