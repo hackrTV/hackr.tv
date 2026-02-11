4e90b4e02d02b0266589de260b676161577ab48f
XERAEN <x@hackr.tv>
2026-02-11 16:41:39 -0600

    Fixes redirects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -38,4 +38,8 @@ class PagesController < ApplicationController
 
   def sector_x
   end
+
+  def not_found
+    render file: Rails.public_path.join("404.html"), layout: false, status: :not_found
+  end
 end
