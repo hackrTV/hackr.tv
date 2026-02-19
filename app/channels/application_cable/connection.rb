c49d97105d477048efe8b2608ef989fc3035b2e1
XERAEN <x@hackr.tv>
2026-02-19 15:59:50 -0600

    Direct Uplink WebSocket Connection for Admins (#173)

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,8 +9,39 @@ module ApplicationCable
     private
 
     def find_hackr
-      # Allow anonymous connections (return nil if not logged in)
-      GridHackr.find_by(id: cookies.encrypted[:grid_hackr_id])
+      # Path 1: Cookie auth (existing)
+      if (hackr = GridHackr.find_by(id: cookies.encrypted[:grid_hackr_id]))
+        return hackr
+      end
+
+      # Path 2: Admin token auth via query params
+      if request.params[:token].present? && request.params[:hackr_alias].present?
+        return find_hackr_by_admin_token
+      end
+
+      nil # anonymous
+    end
+
+    def find_hackr_by_admin_token
+      configured_token = ENV["HACKR_ADMIN_API_TOKEN"]
+      reject_unauthorized_connection unless configured_token.present?
+
+      token = request.params[:token]
+      reject_unauthorized_connection unless token.is_a?(String)
+
+      unless ActiveSupport::SecurityUtils.secure_compare(token, configured_token)
+        Rails.logger.warn("[ActionCable] Invalid admin token from #{request.remote_ip}")
+        reject_unauthorized_connection
+      end
+
+      hackr = GridHackr.find_by(hackr_alias: request.params[:hackr_alias])
+      unless hackr&.admin?
+        Rails.logger.warn("[ActionCable] Token auth denied for non-admin '#{request.params[:hackr_alias]}'")
+        reject_unauthorized_connection
+      end
+
+      Rails.logger.info("[ActionCable] Admin token auth: #{hackr.hackr_alias}")
+      hackr
     end
   end
 end
