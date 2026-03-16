f20b9f9ffb7a086c3599f5764d91fe36a9fea817
XERAEN <x@hackr.tv>
2026-03-16 10:54:54 -0500

    Adds some missing annotations

--- spec/factories/code_repositories.rb
+++ spec/factories/code_repositories.rb
@@ -1,3 +1,33 @@
+# == Schema Information
+#
+# Table name: code_repositories
+# Database name: primary
+#
+#  id               :integer          not null, primary key
+#  default_branch   :string
+#  description      :text
+#  full_name        :string           not null
+#  github_pushed_at :datetime
+#  homepage         :string
+#  language         :string
+#  last_synced_at   :datetime
+#  name             :string           not null
+#  size_kb          :integer          default(0)
+#  slug             :string           not null
+#  stargazers_count :integer          default(0)
+#  sync_error       :text
+#  sync_status      :string
+#  visible          :boolean          default(TRUE), not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  github_id        :integer          not null
+#
+# Indexes
+#
+#  index_code_repositories_on_github_id  (github_id) UNIQUE
+#  index_code_repositories_on_slug       (slug) UNIQUE
+#  index_code_repositories_on_visible    (visible)
+#
 FactoryBot.define do
   factory :code_repository do
     sequence(:name) { |n| "repo-#{n}" }
