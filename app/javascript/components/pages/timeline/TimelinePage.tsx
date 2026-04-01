86a4d2aba081b0ff9cd2e7961734903b2bb0c2b6
XERAEN <x@hackr.tv>
2026-04-01 14:08:14 -0500

    Addresses CR

--- app/javascript/components/pages/timeline/TimelinePage.tsx
+++ app/javascript/components/pages/timeline/TimelinePage.tsx
@@ -104,7 +104,7 @@ export const TimelinePage: React.FC = () => {
         maxWidth: isMobile ? '100%' : '800px',
         margin: '0 auto',
         padding: isMobile ? '0 12px 60px' : '0 24px 80px',
-        marginLeft: isMobile ? 'auto' : 'max(auto, 220px)',
+        marginLeft: isMobile ? 'auto' : '220px',
         position: 'relative'
       }}>
         {/* Page header */}
